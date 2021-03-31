71cc6a80f6f1a7ff5043c71b2314f718e90205a1
Joseph Krill <joe@Josephs-MBP.joekrill.net>
2021-03-31 16:22:57 -0400

    Use chakra-ui

--- packages/client-web/src/index.tsx
+++ packages/client-web/src/index.tsx
@@ -1,14 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { ColorModeScript } from "@chakra-ui/react";
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { theme } from "./features/theme";
+import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
+    <ColorModeScript initialColorMode={theme.config.initialColorMode} />
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function

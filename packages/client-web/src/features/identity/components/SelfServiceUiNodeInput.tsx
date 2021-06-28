e6645d852a1f0efa4ea26a6d895451bce19219d7
Joe Krill <joe@joekrill.com>
2021-06-28 18:03:50 -0400

    More consistent auth form labels

--- packages/client-web/src/features/identity/components/SelfServiceUiNodeInput.tsx
+++ packages/client-web/src/features/identity/components/SelfServiceUiNodeInput.tsx
@@ -13,6 +13,24 @@ export interface SelfServiceUiNodeInputProps {
   isSubmitting: boolean;
 }
 
+/**
+ * A mapping of a field's `name` attribute to the desired label.
+ * Kratos sents inconsistent labels (if any) for various forms. Until that's
+ * fixed or improved, this give us more consistency.
+ */
+const LABELS: Record<string, string> = {
+  password: "Password",
+
+  /**
+   * Kratos uses "ID" here (because it's possible to have multiple, so I guess
+   * it doesn't distinguish). But we know we always want an email address.
+   */
+  password_identifier: "E-Mail",
+
+  // The "recovery" flow doesn't provide a label, for some reason.
+  email: "E-Mail",
+};
+
 export const SelfServiceUiNodeInput = ({
   isSubmitting,
   node,
@@ -27,20 +45,20 @@ export const SelfServiceUiNodeInput = ({
     case "password":
     case "email":
     case "text": {
-      const defaultLabel =
-        attributes.type === "password" ? "Password" : attributes.name;
-
       return (
         <FormControl
           isRequired={attributes.required}
           isDisabled={attributes.disabled || isSubmitting}
+          mb={3}
         >
           <FormLabel>
-            {meta?.label?.text || label?.text || defaultLabel}
+            {LABELS[attributes.name] ||
+              meta?.label?.text ||
+              label?.text ||
+              attributes.name}
           </FormLabel>
           <Input
             {...attributes}
-            bg="white"
             borderColor="gray.300"
             defaultValue={value}
             disabled={attributes.disabled || isSubmitting}

edd2a2c2c16cd24bce120ee5b0d634b29ce9d463
Joe Krill <joe@joekrill.com>
2021-11-27 12:39:53 -0500

    Add tests for requireAuthenticated

--- packages/api-server/jest.config.js
+++ packages/api-server/jest.config.js
@@ -2,5 +2,13 @@ module.exports = {
   projects: ["<rootDir>/test/*"],
   collectCoverage: true,
   coverageProvider: "v8",
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 70,
+      lines: 80,
+      statements: 80,
+    },
+  },
   testEnvironment: "node",
 };
---
+++ packages/api-server/src/features/auth/requireAuthenticated.test.ts
@@ -0,0 +1,37 @@
+import { createMockContext } from "@shopify/jest-koa-mocks";
+import { Middleware, ParameterizedContext } from "koa";
+import { requireAuthenticated } from "./requireAuthenticated";
+
+jest.unmock("./requireAuthenticated");
+
+describe("requireAuthenticated", () => {
+  let instance: Middleware;
+  const nextMock = jest.fn();
+  let contextMock: ParameterizedContext;
+
+  beforeEach(() => {
+    instance = requireAuthenticated();
+  });
+
+  describe("when `state` includes a session", () => {
+    beforeEach(() => {
+      contextMock = createMockContext({ state: { session: {} } });
+      instance(contextMock, nextMock);
+    });
+
+    it("calls next", () => {
+      expect(nextMock).toHaveBeenCalled();
+    });
+  });
+
+  describe("when `state` does not include a session", () => {
+    beforeEach(() => {
+      contextMock = createMockContext({ state: { session: undefined } });
+      instance(contextMock, nextMock);
+    });
+
+    it("throws a 401 error to koa", () => {
+      expect(contextMock.throw).toHaveBeenCalledWith(401);
+    });
+  });
+});

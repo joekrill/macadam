4b86cfb5bca5561228e5a4b5dc454083e8b84521
Joe Krill <joe@joekrill.com>
2023-12-27 15:01:48 -0500

    Fix verifiable addresses selector

--- packages/api-client/src/auth/selectors/selectPendingVerifiableAddresses.ts
+++ packages/api-client/src/auth/selectors/selectPendingVerifiableAddresses.ts
@@ -1,6 +1,13 @@
+import { createSelector } from "@reduxjs/toolkit";
 import { IdentitySliceState } from "../identitySlice";
 
-export const selectPendingVerifiableAddresses = (state: IdentitySliceState) =>
-  state.identity.identity?.verifiable_addresses?.filter(
-    (address) => !address.verified && address.status === "sent",
-  ) || [];
+export const selectPendingVerifiableAddresses = createSelector(
+  [
+    (state: IdentitySliceState) =>
+      state.identity.identity?.verifiable_addresses,
+  ],
+  (addresses) =>
+    addresses?.filter(
+      (address) => !address.verified && address.status === "sent",
+    ) || [],
+);
